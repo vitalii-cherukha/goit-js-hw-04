2d4dbb9026bf3b57f19baa2ce0e37cf4a33aec44
vitalii <vitalii.cherukha@gmail.com>
2025-06-17 19:09:40 +0300

    Fix JS

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,17 @@
-'use strict';
+function isEnoughCapacity(products, containerSize) {
+  const values = Object.values(products);
+  let totalProduct = 0;
 
-function slugify(title) {
-  const lowerTitle = title.toLowerCase();
-  const words = lowerTitle.split(' ');
-  const slug = words.join('-');
-  return slug;
+  for (const value of values) {
+    totalProduct += value;
+  }
+  return totalProduct <= containerSize;
 }
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
